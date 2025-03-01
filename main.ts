4a47472a8f02de37c083efc5fa0a4f42064644ef
cicakukac <wirth.ervin.daniel@gmail.com>
2025-03-01 20:39:16 +0000

    Update main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic{
+    export function plotAt(index: number) : void{
+        index |= 0
+        const x = Math.round(index / 5);
+        const y = Math.round(index % 5);
+        led.plot(x, y);
+    }
+}
